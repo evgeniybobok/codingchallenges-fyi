990b3ae09a163c193cfe823145cbda7c53e8873f
Evgeniy Bobok <evgeniybobok@gmail.com>
2024-09-03 22:18:27 +0200

    add my_wc.py

--- my_wc.py
+++ my_wc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import argparse
 
-def count_lines_words_symbols_bytes(filepath):
+def count_all(filepath):
+    # read the file once, count everything
     with open(filepath, 'r') as f:
         lines = f.readlines()
     
@@ -15,7 +17,29 @@ def count_lines_words_symbols_bytes(filepath):
     return num_lines, num_words, num_chars, num_bytes
 
 def main():
-    print(count_lines_words_symbols_bytes('text.txt'))
+    parser = argparse.ArgumentParser(description="My version of the wc command")
+    parser.add_argument('file_path', type=str, help="Path to the file")
+    parser.add_argument('-l', '--lines', action='store_true', help="Count the number of lines")
+    parser.add_argument('-w', '--words', action='store_true', help="Count the number of words")
+    parser.add_argument('-c', '--chars', action='store_true', help="Count the number of characters")
+    parser.add_argument('-b', '--bytes', action='store_true', help="Count the number of bytes")
+    args = parser.parse_args()
+    # print(args.file_path, args.lines, args.words, args.chars, args.bytes)
+    num_lines, num_words, num_chars, num_bytes = count_all(args.file_path)
+
+    if args.lines:
+        print(f"Lines: {num_lines}")
+    if args.words:
+        print(f"Words: {num_words}")
+    if args.chars:
+        print(f"Characters: {num_chars}")
+    if args.bytes:
+        print(f"Bytes: {num_bytes}")
+
+    if not (args.lines or args.words or args.chars or args.bytes):
+        print("No optional argument provided, printing all:")
+        print(f"Lines: {num_lines}, Words: {num_words}, Characters: {num_chars}, Bytes: {num_bytes}")
+
 
 if __name__ == '__main__':
     main()
